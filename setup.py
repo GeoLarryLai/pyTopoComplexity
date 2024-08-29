099549939c6ddb4e8de1dd87c1a594cb3fa8ce0e
Larry Syu-Heng Lai <larrysyuhenglai@gmail.com>
2024-08-28 23:17:32 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README-pypi.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pytopocomplexity",
-    version="0.8.0",
+    version="0.8.1",
     author="Larry Syu-Heng Lai",
     author_email="larrysyuhenglai@gmail.com",
     description="A package for topographic complexity analysis",
